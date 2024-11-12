deba049bbb936a5d51470fc5387272046db9898a
taco-tortilla <tortillataco0@gmail.com>
2024-11-12 22:44:38 +0900

    update: env

--- notion-db-connector/config/env.go
+++ notion-db-connector/config/env.go
@@ -2,12 +2,21 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
+)
+
+var (
+	DbId      string
+	NotionURL string
 )
 
 func LoadEnv() {
 	if err := godotenv.Load(".env"); err != nil {
 		fmt.Printf("failed to load .env: %v", err)
 	}
+
+	DbId = os.Getenv("NOTION_DB_ID")
+	NotionURL = os.Getenv("NOTION_BASE_URL")
 }
